1733e2a616898fe2035d019dd3d7c5f5fb4b6bd5
margot <cruzmargot2432@gmail.com>
2024-04-02 15:53:12 +0200

    02/04/2023

--- search.php
+++ search.php
@@ -5,7 +5,7 @@ $conn = connect_database();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $searchTerm = trim($_POST["searchTerm"]);
     if (!empty($searchTerm)) {
-        $sql = "SELECT plat.libelle AS plat_libelle, plat.image AS plat_image, 
+        $sql = "SELECT plat.libelle AS plat_libelle, plat.image AS plat_image, plat.description AS plat_description,
                     entree.libelle AS entree_libelle, entree.image AS entree_image, 
                     dessert.libelle AS dessert_libelle, dessert.image AS dessert_image, 
                     categorie.libelle AS categorie_libelle, categorie.image_mobile AS categorie_image
@@ -24,10 +24,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Créer un tableau pour stocker les plats uniques
         $uniquePlats = array();
-
+        
         foreach ($results as $result) {
             $platLibelle = $result['plat_libelle'];
-            // Vérifier si le plat a déjà été ajouté aux résultats
             if (!isset($uniquePlats[$platLibelle])) {
                 // Ajouter le plat aux résultats uniques
                 $uniquePlats[$platLibelle] = $result;
@@ -36,10 +35,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Afficher les résultats uniques
         foreach ($uniquePlats as $plat) {
-            echo "Plat: " . $plat['plat_libelle'] . "<br>";
-            // Ajoutez les autres champs que vous voulez afficher ici...
-            echo "Catégorie: " . $plat['categorie_libelle'] . "<br>";
-            echo "<br>";
+            echo ' 
+            <div class="card">
+                <img src="'. $plat['plat_image'] .'" alt="' . $plat['plat_libelle'] . '">
+                <div class="card-details">
+                    <p>' . $plat['plat_libelle'] . '</p>
+                    <p>' . $plat['plat_description'] . '</p>
+                    <button>Commander</button>
+                </div>
+            </div>
+            ';
+
+            // Afficher les détails de la catégorie
+            echo ' 
+            <div class="card">
+                <img src="'. $plat['categorie_image'] .'" alt="' . $plat['categorie_libelle'] . '">
+                <div class="card-details">
+                    <button>Commander</button>
+                </div>
+            </div>
+            ';
         }
     } else {
         echo "Veuillez entrer un terme de recherche.";
